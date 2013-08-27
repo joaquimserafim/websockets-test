d53e687c58e9e241477c952dd7c1968853b4b662
Joaquim Serafim <joaquim.serafim@gmail.com>
2013-08-28 00:45:50 +0100

    not finish but runs

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 
 var Hapi = require('hapi');
-
 var socketIo = require('socket.io');
 
-
+// server
 var server = Hapi.createServer(8080);
 
-
+// route
 server.route({
     method: 'GET',
     path: '/{path*}',
@@ -15,15 +14,20 @@ server.route({
     }
 });
 
-
-
-
-
+// start server and socket.io
 server.start(function start() {
   console.log('server start at '.concat(server.info.uri));
   var io = socketIo.listen(server.listener);
 
-  io.sockets.on('connection', function conncetion(socket) {
+  io.sockets.on('connection', function connection(socket) {
+    socket.on('disconnect', function disconnect() {
+      io.sockets.emit('user disconnected');
+    });
+    
     socket.emit('start', {msg: 'socket on'});
+
+    socket.on('msg', function(data) {
+      io.sockets.emit('newMsg', data);
+    });
   });
 });
